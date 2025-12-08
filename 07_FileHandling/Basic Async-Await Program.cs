8bed3164cfec4b77cdb81fbcbc7c2f8854f658b4
Gaurav makone <gauravmakone@gmail.com>
2025-12-08 23:52:46 +0530

    Basic Program async and await

---
+++ 07_FileHandling/Basic Async-Await Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class AsyncDemo
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Program started");
+
+        await ShowAsync();   // await  = wait but no block
+
+        Console.WriteLine("Program ended");
+    }
+
+    static async Task ShowAsync()
+    {
+        Console.WriteLine("Async work started...");
+
+        await Task.Delay(2000);   // 2 seconds wait (non-blocking)
+
+        Console.WriteLine("Async work completed");
+    }
+}
